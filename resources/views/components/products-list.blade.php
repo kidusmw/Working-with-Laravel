93df518ebc2a32ec0140dc9d44b0d083c87f8ccd
Kidus Maru <kiduswubie7110@gmail.com>
2024-12-18 22:40:00 +0300

    Add Products list component

---
+++ resources/views/components/products-list.blade.php
@@ -0,0 +1,8 @@
+<div class="container mx-auto px-4 py-8">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div class="bg-white rounded-lg shadow-md p-6">
+            <h2 class="text-xl font-semibold mb-2">{{ $name }}</h2>
+            <p class="text-lg font-bold text-blue-600">{{ $quantity }}</p>
+        </div>
+    </div>
+</div>
--- resources/views/pages/products.blade.php
+++ resources/views/pages/products.blade.php
@@ -1,9 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-    @foreach ($products as $product)
-        <li>
-            {{ $product->name }}
-        </li>
-    @endforeach
+    <h1 class="text-3xl font-bold mb-6 text-center">Our Products</h1>
+    <ul>
+        @foreach ($products as $product)
+            <li>
+                <x-products-list :name="$product['name']" :quantity="$product['quantity']" />
+            </li>
+        @endforeach
+    </ul>
 @endsection
